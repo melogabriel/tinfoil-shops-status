84cccebeb54a41edf74a4492f43462778bd75e5b
Gabriel Melo <46683068+melogabriel@users.noreply.github.com>
2025-04-12 10:37:13 -0300

    Update check_url.py

--- check_url.py
+++ check_url.py
@@ -50,7 +50,7 @@ def generate_readme(results):
         f.write("[![Update Shop Status](https://github.com/melogabriel/tinfoil-shops-status/actions/workflows/update.yml/badge.svg)](https://github.com/melogabriel/tinfoil-shops-status/actions/workflows/update.yml) \n")
         f.write("\n This repository checks the status of the tinfoil shops listed on https://melogabriel.github.io/tinfoil-shops/ every 6 hours. \n")
         f.write("\n If you find it useful to monitor which shops are working, please consider giving this repository a star: https://github.com/melogabriel/tinfoil-shops-status \n")
-        f.write(f"_Last updated: {time.strftime('%Y-%m-%d %H:%M:%S')}_\n\n")
+        f.write(f"\n _Last updated: {time.strftime('%Y-%m-%d %H:%M:%S')}_\n\n")
         f.write("| Host | Status |\n")
         f.write("|------|--------|\n")
         for host, status in results:
