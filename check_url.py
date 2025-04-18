01b0e808093ce471ceb3a01c327ec470f0a73848
Gabriel Melo <46683068+melogabriel@users.noreply.github.com>
2025-04-18 12:11:19 -0300

    Update check_url.py

--- check_url.py
+++ check_url.py
@@ -82,7 +82,7 @@ def generate_readme(results):
     # Timezone-aware last updated
     tz = pytz.timezone(TIMEZONE)
     now = datetime.now(tz)
-    last_updated = now.strftime('%Y-%m-%d %H:%M:%S %OZ')
+    last_updated = now.strftime('%Y-%m-%d %H:%M:%S %Z %OZ')
 
     with open("README.md", "w", encoding="utf-8") as f:
         f.write("#  Tinfoil Shops Status Monitor\n\n")
