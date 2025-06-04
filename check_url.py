67febf114300e7028ef05c95829c4d3d514161ac
Gabriel Melo <46683068+melogabriel@users.noreply.github.com>
2025-06-04 17:51:10 -0300

    Update check_url.py

--- check_url.py
+++ check_url.py
@@ -108,8 +108,8 @@ def generate_readme(results):
     last_updated = now.strftime('%Y-%m-%d %H:%M:%S %OZ%Z')
 
     with open("README.md", "w", encoding="utf-8") as f:
-        f.write("[![Update Shop Status](https://github.com/melogabriel/tinfoil-shops-status/actions/workflows/update.yml/badge.svg)](https://github.com/melogabriel/tinfoil-shops-status/actions/workflows/update.yml)")
-        f.write("![GitHub Repo stars](https://img.shields.io/github/stars/melogabriel/tinfoil-shops-status)")
+        f.write("[![Update Shop Status](https://github.com/melogabriel/tinfoil-shops-status/actions/workflows/update.yml/badge.svg)](https://github.com/melogabriel/tinfoil-shops-status/actions/workflows/update.yml) ")
+        f.write("![GitHub Repo stars](https://img.shields.io/github/stars/melogabriel/tinfoil-shops-status) ")
         f.write("![GitHub watchers](https://img.shields.io/github/watchers/melogabriel/tinfoil-shops-status)\n\n")
         f.write("### Check which tinfoil shops are active and working for Nintendo Switch\n\n")
         f.write("This page monitors the availability of Tinfoil shops from [this source list](https://opennx.github.io) and updates automatically every 6 hours.\n\n")
